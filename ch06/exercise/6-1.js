3c9303436c8c64e6482bd2bdf39b53f8af533a03
optical3mild <chupa.pal.chad@gmail.com>
2019-02-27 17:53:03 +0900

    chapter06

---
+++ ch06/exercise/6-1.js
@@ -0,0 +1,2 @@
+function over(obj) { obj.style.background="yellow";}
+function out(obj) { obj.style.background="white";}
---
+++ ch06/lib.js
@@ -0,0 +1,2 @@
+function over(obj) { obj.src="media/banana.png"}
+function out(obj) { obj.src="media/apple.png"}
